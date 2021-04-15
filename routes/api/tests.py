e9cd1408a16829ba0a2fcfd46e21fc7510262e8c
Chirag <developerchirag09@gmail.com>
2021-04-15 20:29:46 +0530

    Tests added for Routes API.

--- routes/api/tests.py
+++ routes/api/tests.py
@@ -1,3 +1,56 @@
-from django.test import TestCase
+import json
 
-# Create your tests here.
+from django.test import TestCase
+from django.test import Client
+
+
+class RouteTestCase(TestCase):
+    """
+    Route API test for list, create, update and delete actions
+    """
+
+    def setUp(self):
+        self.client = Client()
+        self.path = {
+            "type": "MultiLineString",
+            "coordinates": [[
+                [16.354, 48.27571], [16.35444, 48.27522], [16.35327, 48.27473]
+            ]]
+        }
+
+    def test_routes_api(self):
+        # test route create api
+        response = self.client.post('/api/routes/', data=json.dumps({
+            "name": "Route 1",
+            "paths": self.path
+        }), content_type='application/json')
+        self.assertEqual(response.status_code, 201)
+
+        # test route list api
+        response = self.client.get('/api/routes/', content_type='application/json')
+        data = response.data
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(data), 1)
+        self.assertEqual(data[0]["name"], "Route 1")
+
+        route_id = data[0]["id"]
+
+        # test route update api
+        response = self.client.put(f'/api/routes/{route_id}/', data=json.dumps({
+            "name": "Route 2",
+            "paths": self.path
+        }), content_type='application/json')
+        self.assertEqual(response.status_code, 200)
+
+        # test route get api and verify update
+        response = self.client.get(f'/api/routes/{route_id}/', content_type='application/json')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data["name"], "Route 2")
+
+        # test route delete api
+        response = self.client.delete(f'/api/routes/{route_id}/')
+        self.assertEqual(response.status_code, 204)
+
+        # verify route delete api
+        response = self.client.get(f'/api/routes/{route_id}/', content_type='application/json')
+        self.assertEqual(response.status_code, 404)
